1f00f865439108c3abf8eb7e0b525b89db50ff22
Arnold <junfernando727@gmail.com>
2024-06-24 16:38:53 +0800

    remove section of css

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -33,10 +33,6 @@
 </body>
 @stop
 
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
 
 @section('js')
     <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
